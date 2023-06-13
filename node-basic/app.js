df4064d6579a814200f9e39c380f6663b46afae0
Dan <ngthdan0811@gmail.com>
2023-06-13 18:04:19 +0700

    helper func path

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,29 +1,24 @@
 const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title" />
-      <button type="submit">Add product</button>
-    </form>
-  `);
-});
+app.use(shopRoutes);
+app.use("/admin", adminRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use((req, res, next) => {
+  // res.status(404).send(`
+  //   <h1>Page not Found</h1>
+  // `);
 
-app.use("/", (req, res, next) => {
-  res.send(`
-    <h1>Hello from Express</h1>
-  `);
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
