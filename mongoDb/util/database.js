0486901f455784526b18b2305593fd0713e5661d
Dan <ngthdan0811@gmail.com>
2023-06-21 17:46:18 +0700

    mongodb basic

--- mongoDb/util/database.js
+++ mongoDb/util/database.js
@@ -1,8 +1,29 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (cb) => {
+  MongoClient.connect(
+    "mongodb+srv://thanhdan0811:thanhdan2511@cluster811.aeyzc.mongodb.net/"
+  )
+    .then((client) => {
+      console.log("Connected!!");
+      _db = client.db("shop_node");
+      cb();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "NO database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
