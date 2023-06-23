c8e54f793a6e2c4e96927356ee51447ab0bbaffd
thanhdan0811 <thanhdannguyen@deevotech.com>
2023-06-23 22:03:53 +0700

    mongo add product

--- mongoDb/models/product.js
+++ mongoDb/models/product.js
@@ -2,18 +2,29 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp = null;
+    if (this._id) {
+      // Update Product 
+      dbOp = db.collection('products').updateOne({ _id:  this._id}, {
+        $set: this
+      });
+    } else {
+      dbOp = db
+        .collection("products")
+        .insertOne(this);
+
+    }
+    return dbOp
       .then((result) => {
         console.log(result);
       })
@@ -46,6 +57,17 @@ class Product {
         return product;
       })
       .catch((err) => console.log(err));
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db.collection('products').deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log("DELETED");
+      })
+      .catch(err => {
+        console.log(err)
+      });
   }
 }
 
